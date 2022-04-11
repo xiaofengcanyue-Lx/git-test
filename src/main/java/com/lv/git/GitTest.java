ed13d1701c682a1e71a570374f56422ee0643fb8
xiaofengcanyue-Lx <103411906+xiaofengcanyue-Lx@users.noreply.github.com>
2022-04-11 18:17:40 +0800

    Update GitTest.java

--- src/main/java/com/lv/git/GitTest.java
+++ src/main/java/com/lv/git/GitTest.java
@@ -12,5 +12,6 @@ public class GitTest {
         System.out.println("hot-fix test!");
         System.out.println("push test!");
          System.out.println("push test1111!");
+         System.out.println("update!");
     }
 }
